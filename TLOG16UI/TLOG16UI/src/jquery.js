ed5da31b9a11e04e2ff997c1baca2085c1ee6c7f
kovacskornel <ztoth@precognox.com>
2017-01-09 16:15:48 +0100

    fixed database

--- TLOG16UI/TLOG16UI/src/jquery.js
+++ TLOG16UI/TLOG16UI/src/jquery.js
@@ -4,101 +4,83 @@
  * and open the template in the editor.
  */
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     var actmonth = new Date();
     var year = actmonth.getFullYear();
     var month = actmonth.getMonth();
-    
-    $("#mymonth").text(year + "-" + (month+1));
- 
-        $("#next_month").click(function(){
+    $("#mymonth").text(year + "-" + (month + 1));
+    $("#next_month").click(function () {
         actmonth.addMonths(1);
         var year = actmonth.getFullYear();
         var month = actmonth.getMonth();
-        $("#mymonth").text(year + "-" + (month+1));
+        $("#mymonth").text(year + "-" + (month + 1));
         printCalendar(actmonth);
-
     });
-    
-    $("#mymonth").text(year + "-" + (month+1));
-
-    $("#prev_month").click(function(){
-    actmonth.addMonths(-1);
-    var year = actmonth.getFullYear();
-    var month = actmonth.getMonth();
-    $("#mymonth").text(year + "-" + (month+1));
-    printCalendar(actmonth);
+    $("#mymonth").text(year + "-" + (month + 1));
+    $("#prev_month").click(function () {
+        actmonth.addMonths(-1);
+        var year = actmonth.getFullYear();
+        var month = actmonth.getMonth();
+        $("#mymonth").text(year + "-" + (month + 1));
+        printCalendar(actmonth);
     });
-    
-    function printCalendar(date){
+    function printCalendar(date) {
+
         $("#calendar").empty();
-                        $.ajax({
-			type: 'GET',
-			url: "http://127.0.0.1:9090/tlog-backend/timelogger/workmonths/" + actmonth.getFullYear().toString() + "/" + (actmonth.getMonth() + 1).toString(),
-			contentType: "application/json",
-			dataType: 'jsonp',
-			traditional: true,
-			success: function (json) {
-            
-            }
-    
-            });
         var weekcounter = 0;
         var tr = '<tr>';
-        $("#calendar").css("width","100%");
+        var mytr = $('tr');
+        $("#calendar").css("width", "100%");
         $('<tr><th class = "th">Mon</th><th class = "th">Tue</th><th class = "th">Wed</th><th class = "th">Thu</th><th class = "th">Fri</th><th class = "th">Sat</th><th class = "th">Sun</th></tr>').appendTo('#calendar');
-        var firstDay = new Date(date.getFullYear(),date.getMonth(),0).getDay();
-        if(firstDay<7)
+        var firstDay = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
+        if (firstDay < 7)
         {
-        for(var i=0;i<firstDay;i++)
-        {
-            tr += '<td class="td"></td>';
-            weekcounter++;
-        }
-        }
-
-                var wd =false;
-            
-            
-            
-                  
-        for(var i=0;i<date.getDaysInMonth(date);i++)
-        {
-            
-            
-            tr+= '<td>'+(i+1);
-                       
-            tr+='</td>';
-            weekcounter++;
-            if(weekcounter === 7)
+            for (var i = 0; i < firstDay; i++)
             {
+                tr += '<td class="td"></td>';
+                weekcounter++;
+            }
+        }
+        console.log("Loading json...");
+            $.ajax({
+			type: 'GET',
+			url: "http://127.0.0.1:9090/tlog-backend/timelogger/workmonths/" + actmonth.getFullYear().toString() + "/" + (actmonth.getMonth() + 1).toString(),
+			contentType: 'application/json',
+            traditional: true,
+            error: console.log("Failed loading: " + "http://127.0.0.1:9090/tlog-backend/timelogger/workmonths/" + actmonth.getFullYear().toString() + "/" + (actmonth.getMonth() + 1).toString()),
+            success: function (data) {
                 
-              tr+= '</tr>';
-              $(tr).appendTo('#calendar');
-              tr = '<tr>';
-              weekcounter = 0;
-            }
+                for (var i = 0; i < date.getDaysInMonth(date); i++)
+                {
 
-        }
-        if(weekcounter !== 0)
-        {
-        for(var i = weekcounter;i<7;i++)
-        {
-            
-            
-            tr += '<td class="td"></td>';
-        }
-        
-        $(tr).appendTo('#calendar');
+                    tr += '<td>' + (i + 1);
+                    tr += '</td>';
+                    weekcounter++;
+                    if (weekcounter === 7)
+                    {
 
+                        tr += '</tr><tr>';
+                        weekcounter = 0;
+                    }
+
+                }
             }
-   
-       
-       
-    
+        });
+
+        if (weekcounter !== 0)
+        {
+            for (var i = weekcounter; i < 7; i++)
+            {
+                tr += '<td class="td"></td>';
+            }
+            tr += '</tr>';
+        }
+        mytr.appendTo('#calendar');
+        console.log("Finished printing!");
+
     }
-            
-   
+
     printCalendar(actmonth);
-    });
+
+});
